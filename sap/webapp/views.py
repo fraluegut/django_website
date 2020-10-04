1a9f5fe2b771242785f1eb8d5a0ab875e0a1b3fe
fraluegut <franciscojavierluengo@outlook.com>
2020-10-04 22:39:06 +0200

    Añadiendo tablas dinámicas

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -2,5 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from personas.models import Persona
+
+
 def bienvenido(request):
-    return render(request, 'bienvenido.html')
+    no_personas_var = Persona.objects.count()
+    personas = Persona.objects.all()
+    return render(request, 'bienvenido.html', {'no_personas':no_personas_var, 'personas': personas})
